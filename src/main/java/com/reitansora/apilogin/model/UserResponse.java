496a72924ab45584670fac80047f9d9e1e656990
ReitanSora <stivenpilca@gmail.com>
2024-07-20 14:29:35 -0500

    fix: change the secured endpoint and jwt token
    The secured endpoint now return user'data.
    The JWT token also contain the nickname of the user.

---
+++ src/main/java/com/reitansora/apilogin/model/UserResponse.java
@@ -0,0 +1,15 @@
+package com.reitansora.apilogin.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserResponse {
+
+    private final Long id;
+    private final String username;
+    private final String email;
+    private final String password;
+
+}
